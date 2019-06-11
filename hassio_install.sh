613d42114e036671be3659cf61ab51c6da6c6432
Raa'Shaun H <CardcaptorRaaShaun@gmail.com>
2019-06-10 23:49:17 -0400

    Fix for armv7 Supervisor
    This is my proposed fix for issue #15.

--- hassio_install.sh
+++ hassio_install.sh
@@ -70,13 +70,21 @@ case $ARCH in
         HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
         HASSIO_DOCKER="$DOCKER_REPO/amd64-hassio-supervisor"
     ;;
-    "arm" | "armv7l" | "armv6l")
+    "arm" |"armv6l")
         if [ -z $MACHINE ]; then
             echo "[ERROR] Please set machine for $ARCH"
             exit 1
         fi
         HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
         HASSIO_DOCKER="$DOCKER_REPO/armhf-hassio-supervisor"
+    ;;
+    "armv7l")
+        if [ -z $MACHINE ]; then
+            echo "[ERROR] Please set machine for $ARCH"
+            exit 1
+        fi
+        HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
+        HASSIO_DOCKER="$DOCKER_REPO/armv7-hassio-supervisor"
     ;;
     "aarch64")
         if [ -z $MACHINE ]; then
