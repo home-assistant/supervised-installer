e35a8552b499135296616e032fcbfb4824b27c45
Richard Tirtadji <rtirtadji@gmail.com>
2020-05-11 20:09:34 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,12 +12,12 @@ warn "If you want more control over your own system, run"
 warn "Home Assistant as a VM or run Home Assistant Core"
 warn "via a Docker container."
 warn ""
-echo 'Please typ "not supported" to continue this installation'
+echo 'Please type "not supported" to continue this installation'
 read x
 if [ "$x" != "not supported" ]
 then
   echo "OK, bye!"
-  exit 1
+  exit 1;
 fi
 
 ARCH=$(uname -m)
