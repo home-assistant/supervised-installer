128dfdefc800be60cdc2c8039068ea62b074ac72
Franck Nijhof <git@frenck.dev>
2020-06-09 20:43:23 +0200

    Remove confirmation as it breaks piped runs (#101)

--- installer.sh
+++ installer.sh
@@ -12,13 +12,6 @@ warn "If you want more control over your own system, run"
 warn "Home Assistant as a VM or run Home Assistant Core"
 warn "via a Docker container."
 warn ""
-echo 'Please typ "not supported" to continue this installation'
-read x
-if [ "$x" != "not supported" ]
-then
-  echo "OK, bye!"
-  exit 1
-fi
 
 ARCH=$(uname -m)
 DOCKER_BINARY=/usr/bin/docker
