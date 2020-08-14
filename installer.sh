3df59fffdefa65a8301e9af1a6ef39caecf4c023
Pascal Vizeli <pvizeli@syshack.ch>
2020-08-14 09:51:03 +0200

    Cleanup

--- installer.sh
+++ installer.sh
@@ -112,10 +112,6 @@ case $ARCH in
     ;;
 esac
 
-if [ -z "${HOMEASSISTANT_DOCKER}" ]; then
-    error "Found no Home Assistant Docker images for this host!"
-fi
-
 if [[ ! "intel-nuc odroid-c2 odroid-n2 odroid-xu qemuarm qemuarm-64 qemux86 qemux86-64 raspberrypi raspberrypi2 raspberrypi3 raspberrypi4 raspberrypi3-64 raspberrypi4-64 tinker" = *"${MACHINE}"* ]]; then
     error "Unknown machine type ${MACHINE}!"
 fi
