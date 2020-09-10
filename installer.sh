c384c3bee277dd645788b3a72641884ca8fe8352
Pascal Vizeli <pvizeli@syshack.ch>
2020-09-10 10:22:54 +0200

    strict machine check (#110)
    * strict machine check
    
    * full match

--- installer.sh
+++ installer.sh
@@ -137,7 +137,7 @@ case $ARCH in
     ;;
 esac
 
-if [[ ! "intel-nuc odroid-c2 odroid-n2 odroid-xu qemuarm qemuarm-64 qemux86 qemux86-64 raspberrypi raspberrypi2 raspberrypi3 raspberrypi4 raspberrypi3-64 raspberrypi4-64 tinker" = *"${MACHINE}"* ]]; then
+if [[ ! "${MACHINE}" =~ ^(intel-nuc|odroid-c2|odroid-n2|odroid-xu|qemuarm|qemuarm-64|qemux86|qemux86-64|raspberrypi|raspberrypi2|raspberrypi3|raspberrypi4|raspberrypi3-64|raspberrypi4-64|tinker)$ ]]; then
     error "Unknown machine type ${MACHINE}!"
 fi
 
