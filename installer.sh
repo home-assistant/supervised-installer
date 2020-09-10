b2665aa37ecf18d6f900aaae3d6b8cf28c3ba5ad
Pascal Vizeli <pvizeli@syshack.ch>
2020-09-10 10:07:13 +0200

    full match

--- installer.sh
+++ installer.sh
@@ -137,7 +137,7 @@ case $ARCH in
     ;;
 esac
 
-if [[ ! "${MACHINE}" =~ (intel-nuc|odroid-c2|odroid-n2|odroid-xu|qemuarm|qemuarm-64|qemux86|qemux86-64|raspberrypi|raspberrypi2|raspberrypi3|raspberrypi4|raspberrypi3-64|raspberrypi4-64|tinker) ]]; then
+if [[ ! "${MACHINE}" =~ ^(intel-nuc|odroid-c2|odroid-n2|odroid-xu|qemuarm|qemuarm-64|qemux86|qemux86-64|raspberrypi|raspberrypi2|raspberrypi3|raspberrypi4|raspberrypi3-64|raspberrypi4-64|tinker)$ ]]; then
     error "Unknown machine type ${MACHINE}!"
 fi
 
