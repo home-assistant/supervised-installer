0b34e37740c5d6ad1170d01468eaaecf8060824c
Kanga-Who <jason.reibelt@gmail.com>
2020-09-11 17:52:33 +1000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,8 +4,8 @@ systemctl status ModemManager | grep 'active (running)' > /dev/null 2>&1
 
 if [ $? != 0 ]
 then
-        systemctl disable ModemManager > /dev/null
-        systemctl stop ModemManager > /dev/null
+        sudo systemctl disable ModemManager > /dev/null
+        sudo systemctl stop ModemManager > /dev/null
 fi
 
 set -e
